4c355c27636bfb3b8f5114a71a3b8b58e54737a7
obeidallahnouicer <obeidallah.nouicer@esprit.tn>
2026-02-07 22:21:23 +0100

    Add documentation for infrastructure layer package

---
+++ app/infrastructure/__init__.py
@@ -0,0 +1,7 @@
+"""
+Infrastructure layer package.
+
+Contains concrete implementations (adapters) of the ports
+defined in the domain layer. This is where databases, APIs,
+ML models, and other external integrations live.
+"""
